13f2dcca7491d958f1c5395985ad7c47662936bf
AiceKerime <rafiizzaturohman06@gmail.com>
2022-09-29 20:36:26 +0700

    BREAD

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const dataFile = data
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public'));
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
@@ -18,6 +20,11 @@ app.get('/', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add')
+})
+
+app.post('/add', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
 })
 
 app.get('/edit', (req, res) => {
